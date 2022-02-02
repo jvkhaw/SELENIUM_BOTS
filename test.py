03d78273088b82d3918382efa490e06406f379a6
unknown <jv.khaw2896@gmail.com>
2022-02-02 22:48:04 +0800

    Tried to fix loop

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -7,134 +6,165 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException,StaleElementReferenceException,TimeoutException
 
-class SELENIUM_BOT:
-	def __init__(self,
-		driver_path,
-		binary_path
-		):
+driver_path = "C:/Users/ruben/Desktop/SELENIUM_BOTS/chromedriver.exe" # location of your chromedriver
+brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe" # location of your Brave browser
 
-		s = Service(driver_path)
+s = Service(driver_path)
 
-		option = webdriver.ChromeOptions()
-		option.add_argument('log-level=3')
-		option.add_experimental_option('excludeSwitches', ['enable-logging'])
-		option.binary_location = brave_path
-				
-		d = DesiredCapabilities.CHROME
-		d['goog:loggingPrefs'] = { 'browser':'ALL' }
-		
-		self.driver = webdriver.Chrome(service=s, options=option, desired_capabilities=d)
-	
-	def goto(self,website):
-		self.driver.get(website)
+option = webdriver.ChromeOptions()
+option.add_argument('log-level=3')
+option.add_experimental_option('excludeSwitches', ['enable-logging'])
+option.binary_location = brave_path
 
-	def wait_to_click_xpath(self,xpath,wait):
-		element = WebDriverWait(
-			self.driver,
-			wait,
-			ignored_exceptions=(NoSuchElementException,StaleElementReferenceException)).until(
-				EC.element_to_be_clickable((By.XPATH,xpath)))
-		time.sleep(1)
-		element.click()
+d = DesiredCapabilities.CHROME
+d['goog:loggingPrefs'] = { 'browser':'ALL' }
 
-class Alien_Worlds_Bot(SELENIUM_BOT):
-	def __init__(self,
-		driver_path,
-		binary_path):
-		super().__init__(driver_path,
-		binary_path)
-
-	def WAX_login(self):
-		self.goto("https://wallet.wax.io/")
-		self.wait_to_click_xpath('//*[@id="discord-social-btn"]',10)
-
-		# print('Waiting for QR code: ...')
-		
-
-		# https://discord.com/oauth2/authorize
-		print('Waiting for 2FA: ...')
-
-		while True:
-			if self.driver.current_url.startswith("https://wallet.wax.io/dashboard"):
-				print('Logged In Wax!')
-				break;
-			time.sleep(1)
-
-	def AW_login(self):
-		self.goto("https://play.alienworlds.io/")		
-		self.wait_to_click_xpath('//*[@id="root"]/div[3]/div/div[1]/div/div',10)
-		self.switch_window()
-		self.wait_to_click_xpath('//*[@id="root"]/div/section/div[2]/div/div[6]/button',10)
-		self.switch_window()
+driver = webdriver.Chrome(service=s, options=option, desired_capabilities=d)
 
 
-		while True:
-			if self.driver.current_url.startswith("https://play.alienworlds.io/inventory"):
-				print('Logged In Alien Worlds!')
-				break;
-			time.sleep(1)
+wait = WebDriverWait(driver,60)
+def go_to_wax():
+    try:
+        driver.get("https://wallet.wax.io/")
+        return True
+    except AssertionError as e:
+        print(f"Couldnt go to WAX. Error:\n{e}")
+        return False
 
-	def mine(self):
-		# Mine
-		print('Mining...')
-		self.wait_to_click_xpath('//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div/div/div',10)
-		# Claim
-		time.sleep(5)
-		print('Claiming mine...')
-		self.wait_to_click_xpath('//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div/div/div',20)
-		# Authorize wax wallet
-		self.switch_window()
-		self.wait_to_click_xpath('//*[@id="root"]/div/section/div[2]/div/div[5]/button',10)
-		self.switch_window()
+def click_discord_button():    
+    try:
+        discord_button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="discord-social-btn"]')))
+        discord_button.click()
+        return True
+    except AssertionError as e:
+        print(f"Couldnt click discord button. Error:\n{e}")
+        return False
+    
+def login_to_wax_dashboard():    
+    url_to_be = wait.until(EC.url_to_be("https://wallet.wax.io/dashboard"))
+    return url_to_be
 
-		# hours = self.driver.find_elements(
-		# 	By.XPATH,
-		# 	'/html/body/div/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[1]')[0].text
-		
-		time.sleep(20)
-		
-		hours = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[1]')
-		hours = hours.text
+def go_to_aw():
+    try:
+        driver.get("https://play.alienworlds.io/")
+        return True
+    except AssertionError as e:
+        print(f"Couldnt go to AW. Error:\n{e}")
+        return False
+    
+def click_start_now():
+    try:
+        start_now_button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div[3]/div/div[1]/div/div')))
+        start_now_button.click()
+        return True
+    except AssertionError as e:
+        print(f"Couldnt click Start Now button. Error:\n{e}")
+        return False
+    
+def approve_window():
+    window_is_opened = wait.until(EC.new_window_is_opened(driver.window_handles))
+    return window_is_opened
 
-		# minutes = self.driver.find_element() WebDriverWait(self.driver, 20).until(
-		# 	EC.visibility_of_element_located(
-		minutes = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[2]')
-		minutes = minutes.text
-		# 	)
-		# ).get_attribute("text")
+def click_approve():
+    main_window_handle = driver.current_window_handle
+    for wh in driver.window_handles:
+        if wh != main_window_handle:
+            driver.switch_to.window(wh)
+            break
+    try:
+        start_now_button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/section/div[2]/div/div[6]/button')))
+        start_now_button.click()
+        driver.switch_to.window(main_window_handle)
+        return True
+    except AssertionError as e:
+        print(f"Couldnt click Approve button. Error:\n{e}")
+        return False
+    
+def click_mine():
+    try:
+        mine_button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div/div')))
+        mine_button.click()
+        return True
+    except AssertionError as e:
+        print(f"Couldnt click discord button. Error:\n{e}")
+        return False
 
-		# seconds = WebDriverWait(self.driver, 20).until(
-		# 	EC.visibility_of_element_located(
-		seconds = self.driver.find_element(By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[3]')
-		seconds = seconds.text
-		# 	)
-		# ).get_attribute("text")
+def waiting_stale():
+    staleness = wait.until(EC.staleness_of(
+        driver.find_element(By.XPATH,'//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div/div'))
+                          )
+    return staleness
 
-		# print(hours,minutes,seconds)
-		total_time_in_seconds =  int(hours) * 60 * 60 + int(minutes) * 60 + int(seconds)
-		print(total_time_in_seconds)
-		return total_time_in_seconds
+def sign_window():
+    window_is_opened = wait.until(EC.new_window_is_opened(driver.window_handles))
+    return window_is_opened
 
-	def switch_window(self):
-		while len(self.driver.window_handles) == 1:
-			time.sleep(1)	
-		main_window_handle = self.driver.current_window_handle
-		for wh in self.driver.window_handles:
-			if wh != main_window_handle:
-				self.driver.switch_to.window(wh)
-				break;		
+def click_sign():
+    main_window_handle = driver.current_window_handle
+    for wh in driver.window_handles:
+        if wh != main_window_handle:
+            driver.switch_to.window(wh)
+            break
+    try:
+        sign_button = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="root"]/div/section/div[2]/div/div[5]/button')))
+        sign_button.click()
+        driver.switch_to.window(main_window_handle)
+        return True
+    except AssertionError as e:
+        print(f"Couldnt click discord button. Error:\n{e}")
+        return False
+    
+def wait_for_time():    
+    while True:
+        try:        
+            hours = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[1]')))
+            minutes = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[2]')))
+            seconds = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[5]/div[2]/p[1]/span[3]')))
+        # do stuff
+        except TimeoutException:
+            print('Couldnt get time... refreshing')
+            driver.refresh()
+        else:
+            break
 
+    time_available = int(hours.text) * 60 * 60 + int(minutes.text) * 60 + int(seconds.text)
+    return time_available
 
-if __name__ == "__main__":
-	driver_path = "C:/Driver/chromedriver.exe" # location of your chromedriver
-	brave_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe" # location of your Brave browser
+def get_cpu_trilium():
+    trilium = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div[1]/div/div[3]/div[1]/div/div[2]/p[1]')))
+    cpu = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div[10]/div[1]/div[1]/p[2]')))
+    print(f'Trilium: {trilium}   CPU: {cpu}')
 
-	awbot=Alien_Worlds_Bot(driver_path=driver_path,
-		binary_path=brave_path)
-	awbot.WAX_login()
-	awbot.AW_login()
-	while True:
-		mining_time = awbot.mine()
-		time.sleep(mining_time)
+print('Going to wax website...',end=' ')
+print(go_to_wax())
+print('Clicking Discord button...',end=' ')
+print(click_discord_button())
+print('Authorizing Discord & 2FA...',end=' ')
+print(login_to_wax_dashboard())
+print('Going to Alien Worlds...',end=' ')
+print(go_to_aw())
+print('Clicking Start Now...',end=' ')
+print(click_start_now())
+print('Waiting for approve window...',end=' ')
+print(approve_window())
+print('Clicking wallet...',end=' ')
+print(click_approve())
+while True:
+    print('Clicking mine...',end=' ')
+    print(click_mine())
+    print('Waiting Mine staleness',end=' ')
+    print(waiting_stale())
+    print('Claiming mine...',end=' ')
+    print(click_mine())
+    print('Signing window...',end=' ')
+    print(sign_window())
+    print('Clicking sign...',end=' ')
+    print(click_sign())
+    get_cpu_trilium()
+    print('Waiting for time to appear...',end=' ')
+    waiting_time = wait_for_time()
+    print(waiting_time)
+    time.sleep(waiting_time)
+    
